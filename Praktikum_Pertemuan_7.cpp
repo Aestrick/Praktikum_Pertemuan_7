38d0ea50451544130833fb245c9dab089fa7db44
Aestrick <nashrul.fikri19@gmail.com>
2024-04-22 12:42:49 +0700

    Add project files.

---
+++ Praktikum_Pertemuan_7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class bangunDatar{
+
+	//akses modifier
+    private:
+		float panjang, lebar;
+    public:
+		float luas;
+
+		void input() { // methode input 
